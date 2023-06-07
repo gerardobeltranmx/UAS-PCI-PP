4b1569aa65e355fcd31b22285b8574db32fae2d3
Gerardo Beltran <contacto@gerardobeltran.mx>
2023-06-07 13:25:20 -0700

    --

--- 03 Programacion con OpenMP/02 Gestion de datos/prog-08.c
+++ 03 Programacion con OpenMP/02 Gestion de datos/prog-08.c
@@ -4,12 +4,15 @@
 int main( int argc, char *argv[] ){
 	int i=0, N=10, suma=0, idHilo;
    int datos[]={1,2,3,4,5,6,7,8,9,10};
-
-   #pragma omp parallel for shared(suma) private(i, idHilo)
-      for(i = 0; i < N; i++ ) {
-        suma+=datos[i];
-      }
-
+   #pragma omp parallel shared(suma) private(i, idHilo)
+   {
+      #pragma omp parallel for 
+        for(i = 0; i < N; i++ ) {
+          idHilo=omp_get_thread_num(); 
+          printf("El hilo de la operación es: %d\n", idHilo);
+          suma+=datos[i];
+        }
+   }
    printf(" la suma es: %d \n", suma);
 
 
