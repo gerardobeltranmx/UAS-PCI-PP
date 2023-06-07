3f2084664bc4f6a7709740ec604bb7ae6d3e4503
Gerardo Beltran <contacto@gerardobeltran.mx>
2023-06-07 11:43:39 -0700

    --

--- 03 Programacion con OpenMP/02 variables/prog-08.c
+++ 03 Programacion con OpenMP/02 variables/prog-08.c
@@ -1,14 +1,14 @@
-// Distrución de tareas con for y lastprivate
+// Distrución de tareas con for y uso de variables
 #include <stdio.h>
 #include <omp.h>
 int main( int argc, char *argv[] ){
-	int i=0, N=9, suma=0, idHilo;
-   int datos[]={1,2,3,4,5,6,7,8,9};
+	int i=0, N=10, suma=0, idHilo;
+   int datos[]={1,2,3,4,5,6,7,8,9,10};
 
-   #pragma omp parallel for private(i, idHilo)
+   #pragma omp parallel for shared(suma) private(i, idHilo)
       for(i = 0; i < N; i++ ) {
         suma+=datos[i];
-       }
+      }
 
    printf(" la suma es: %d \n", suma);
 
